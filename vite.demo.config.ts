3d4bd59c741f90aeec53fbdf54ae5236f1030d44
jan-cermak-1 <jan.cermak@emplifi.io>
2025-11-14 20:17:09 +0100

    Add base path for GitHub Pages

--- vite.demo.config.ts
+++ vite.demo.config.ts
@@ -5,6 +5,7 @@ import { resolve } from 'path';
 export default defineConfig({
   plugins: [react()],
   root: 'demo',
+  base: '/rockenroll/',
   build: {
     outDir: '../dist-demo',
     emptyOutDir: true,
